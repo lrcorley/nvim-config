0302b16f5582ca713a251c856346b8f3e3d12ece
Logan Corley <logan@logancorley.xyz>
2026-08-24 19:51:12 -0400

    Add rose pine and xcode color schemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -5,5 +5,20 @@ return {
   'xero/miasma.nvim',
   'folke/tokyonight.nvim',
   'olivercederborg/poimandres.nvim',
+  {
+    'rose-pine/neovim',
+    config = function()
+      require('rose-pine').setup({
+        dim_inactive_windows = true,
+        styles = {
+          italic = false,
+          transparency = false
+        }
+      })
+
+      vim.cmd('colorscheme rose-pine')
+    end
+  },
+  { 'lunacookies/vim-colors-xcode' },
   { 'vague-theme/vague.nvim', opts = { italic = false } }
 }
