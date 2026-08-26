c840197d31db44964aa9f643b6859b6b6b93f2fc
Logan Corley <logan@logancorley.xyz>
2026-08-26 18:53:25 -0400

    Add slimline

---
+++ lua/plugins/slimline.lua
@@ -0,0 +1,18 @@
+return {
+  'sschleemilch/slimline.nvim',
+  opts = {
+    spaces = {
+      components = '',
+      left = '',
+      right = '',
+    },
+    sep = {
+      hide = {
+        first = true,
+        last = true,
+      },
+      left = "",
+      right = "",
+    }
+  }
+}
